782cac5a54cd03d60b0d870e2678276a6d64eee0
Vamshi Krishna Sirivolu <c-vamshikrishna.sirivolu@charter.com>
2024-04-24 19:22:38 -0600

    Added instance type

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t.micro"
 
   tags = {
     Name = "HelloWorld"
